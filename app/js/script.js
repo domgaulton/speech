047b9f33fc80d35dbcf5b168d06eedccdf826792
Dom Gaulton <dom.gaulton@readingroom.com>
2018-03-09 16:41:48 +0000

    youtube ID returned from voice

--- app/js/script.js
+++ app/js/script.js
@@ -18,11 +18,13 @@ var clickMe = document.querySelector('.click-me');
 var diagnostic = document.querySelector('.output');
 var transcript = document.querySelector('.transcript');
 var bg = document.querySelector('html');
+var speech;
 
 clickMe.onclick = function() {
-	recognition.start();
+	
 	console.log('Listening...');
 	diagnostic.innerHTML = 'Listening...';
+	recognition.start();
 }
 
 recognition.onresult = function(event) {
@@ -34,18 +36,20 @@ recognition.onresult = function(event) {
 	// These also have getters so they can be accessed like arrays.
 	// The [0] returns the SpeechRecognitionAlternative at position 0.
 	// We then return the transcript property of the SpeechRecognitionAlternative object
+
 	console.log(event);
 
 	// var last = event.results.length - 1;
 	// var speech = event.results[last][0].transcript;
 	
 	var speech = event.results[0][0].transcript;
-	// var speech = 'hello';
 
 	diagnostic.textContent = 'Result received: ' + speech + '. Confidence Level: ' + event.results[0][0].confidence;
 	transcript.textContent = speech;
-	bg.style.backgroundColor = speech;
-	console.log('Confidence: ' + event.results[0][0].confidence);
+	// bg.style.backgroundColor = speech;
+	// console.log('Confidence: ' + event.results[0][0].confidence);
+
+	search(speech);
 }
 
 recognition.onspeechend = function() {
@@ -57,6 +61,51 @@ recognition.onnomatch = function(event) {
 }
 
 recognition.onerror = function(event) {
+	// console.log(event.error);
 	diagnostic.textContent = 'Error occurred: ' + event.error;
+
 }
 
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+// function showResponse(response) {
+//     var responseString = JSON.stringify(response, '', 2);
+//     document.getElementById('response').innerHTML += responseString;
+// }
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See http://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyACNt99PoY7u7F7qWZY5eRIRO2ag6HRB-o');
+
+    // search();
+}
+
+function search(result) {
+	console.log('search'+result);
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: result
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    // showResponse(response);
+    // console.log(response);
+    var id = response.items[0].id.videoId;
+    console.log(id);
+}
